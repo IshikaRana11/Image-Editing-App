53014c9b8586eea13e350a7b5760525eba7451df
Ishika-Rana <ishikaishik11@gmail.com>
2025-01-31 16:44:02 +0530

    Front Page Updated successfully

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import styles from "./Navbar.module.css";
-import classNames from "classnames";
+import React from 'react';
+import styles from './Navbar.module.css';
+import classNames from 'classnames';
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <img src="/images/logo.jpeg" alt="Logo Image" className={styles.logo} />
-      <div className={styles.btns}>
+      <img src='/images/logo.jpeg' alt='Logo Image' className={styles.logo} />
+      {/* <div className={styles.btns}>
         <button className={classNames(styles.roundedBtn, styles.logbtn)}>
           Log in
         </button>
         <button className={classNames(styles.roundedBtn, styles.signbtn)}>
           Sign up
         </button>
-      </div>
+      </div> */}
     </nav>
   );
 };
